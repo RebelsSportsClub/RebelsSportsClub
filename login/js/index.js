2940a0480ed9fcc3c7660574e4c8be3b53cdcbaa
Rebels Sports Club <sportsclub.rebels@gmail.com>
2017-11-09 12:40:06 -0600

    js

--- login/js/index.js
+++ login/js/index.js
@@ -18,7 +18,7 @@ return false;
 }
 else{
 attempt --;// Decrementing by one.
-alert("You have left "+attempt+" attempt;");
+alert("You have left "+ attempt +" attempt;");
 // Disabling fields after 3 attempts.
 if( attempt == 0){
 document.getElementById("username").disabled = true;
